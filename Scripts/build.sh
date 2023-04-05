6280ca69f6f411075e016b22733fe53c89b1f291
chrisakira <chris.akira.maekawa@gmail.com>
2023-04-06 00:22:08 +0100

    First Commit

---
+++ Scripts/build.sh
@@ -0,0 +1,6 @@
+clear
+sudo docker stack deploy -c /home/<Meu Usuario>/Pi-Swarm/traefik/docker-compose.yml traefik  
+sudo docker stack deploy -c /home/<Meu Usuario>/Pi-Swarm/grafana/docker-compose.yml traefik    
+sudo docker stack deploy -c /home/<Meu Usuario>/Pi-Swarm/wordpress/docker-compose.yml  traefik
+sudo docker stack deploy -c /home/<Meu Usuario>/Pi-Swarm/Who/docker-compose.yml  traefik
+ 
