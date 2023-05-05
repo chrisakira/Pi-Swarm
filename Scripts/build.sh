898f70eda273cf0e1efb3bb66759dafc4a47c4df
chrisakira <chris.akira.maekawa@gmail.com>
2023-05-05 19:17:30 +0000

    algumas alterações dsdd

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 clear
-source /root/Pi-Swarm/config.env
+   
+# sudo docker-compose -f ./node-red/docker-compose.yml  up --build -d  
 
-if [ $1 = "Local" ]; then
-    echo "Executando localmente"
-    sudo docker stack deploy -c ./traefik/docker-compose-local.yml traefik  
-    sudo docker stack deploy -c ./grafana/docker-compose-local.yml traefik    
-    sudo docker stack deploy -c ./wordpress/docker-compose-local.yml  traefik
-    sudo docker stack deploy -c ./Who/docker-compose-local.yml  traefik
-    sudo docker-compose -f /home/vm2/Home-Lab/mysql/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/AD_Guard/docker-compose.yml up -d 
-    sudo docker-compose -f /home/vm2/Home-Lab/influx/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/portainer/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/node-red/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/zomboid-dedicated-server/docker-compose.yaml up -d
-else
-    echo "Executando em cluster"
-    sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
-    sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
-    sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik
-    sudo docker stack deploy -c ./Who/docker-compose.yml  traefik
-    sudo docker-compose -f /home/vm2/Home-Lab/mysql/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/AD_Guard/docker-compose.yml up -d 
-    sudo docker-compose -f /home/vm2/Home-Lab/influx/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/portainer/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/node-red/docker-compose.yml  up --build -d
-    sudo docker-compose -f /home/vm2/Home-Lab/zomboid-dedicated-server/docker-compose.yaml up -d
-fi
+# sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
+# sudo docker stack deploy -c ./portainer/docker-compose.yml traefik  
+
+
+sudo docker stack deploy -c ./Who/docker-compose.yml  traefik
+sudo docker stack deploy -c ./mysql/docker-compose.yml traefik  
+sudo docker stack deploy -c ./influxdb/docker-compose.yml traefik  
+sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
+sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
+sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik
+sudo docker stack deploy -c ./passbolt/docker-compose.yaml  traefik
+
+
+sudo systemctl start systemd-resolved
+sudo systemctl enable systemd-resolved
