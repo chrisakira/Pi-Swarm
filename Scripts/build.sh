58625187be171611341a6818455305e2654a3247
chrisakira <chris.akira.maekawa@gmail.com>
2023-05-06 00:23:05 +0000

    Guuguru adicionado

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,17 +4,14 @@ clear
 # sudo docker-compose -f ./node-red/docker-compose.yml  up --build -d  
 
 # sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
-# sudo docker stack deploy -c ./portainer/docker-compose.yml traefik  
-
-
+sudo docker stack deploy -c ./portainer/docker-compose.yml traefik 
 sudo docker stack deploy -c ./Who/docker-compose.yml  traefik
 sudo docker stack deploy -c ./mysql/docker-compose.yml traefik  
 sudo docker stack deploy -c ./influxdb/docker-compose.yml traefik  
 sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
 sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
 sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik
-sudo docker stack deploy -c ./passbolt/docker-compose.yaml  traefik
-
-
-sudo systemctl start systemd-resolved
-sudo systemctl enable systemd-resolved
+sudo docker stack deploy -c ./bitwarden/docker-compose.yml  traefik
+sudo docker stack deploy -c ./redis/docker-compose.yml  traefik
+sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik
+ 
