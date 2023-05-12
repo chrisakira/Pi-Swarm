48806c6168201cf199e9bd42aa3cdf94731e61c7
chrisakira <chris.akira.maekawa@gmail.com>
2023-05-12 21:57:21 +0000

    Random Shit GO

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,15 +3,17 @@ clear
    
 # sudo docker-compose -f ./node-red/docker-compose.yml  up --build -d  
 
-# sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
+sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
 sudo docker stack deploy -c ./portainer/docker-compose.yml traefik 
 sudo docker stack deploy -c ./Who/docker-compose.yml  traefik
 sudo docker stack deploy -c ./mysql/docker-compose.yml traefik  
 sudo docker stack deploy -c ./influxdb/docker-compose.yml traefik  
 sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
 sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
-sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik
+sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik 
+sudo docker stack deploy -c ./wordpress-marquinhos/docker-compose.yml  traefik
 sudo docker stack deploy -c ./bitwarden/docker-compose.yml  traefik
 sudo docker stack deploy -c ./redis/docker-compose.yml  traefik
 sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik
+ 
  
