95a11baca49e4a9ced8f99cf13bcd06ffb8eb794
chrisakira <chris.akira.maekawa@gmail.com>
2024-06-08 14:20:20 +0000

    Potato

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 clear
-   
-# sudo docker-compose -f ./node-red/docker-compose.yml  up --build -d  
+    
 
 sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
 sudo docker stack deploy -c ./portainer/docker-compose.yml traefik 
 sudo docker stack deploy -c ./Who/docker-compose.yml  traefik
 sudo docker stack deploy -c ./mysql/docker-compose.yml traefik  
+sudo docker stack deploy -c ./mariaDB/docker-compose.yml traefik  
 sudo docker stack deploy -c ./influxdb/docker-compose.yml traefik  
 sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
 sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
@@ -14,6 +14,10 @@ sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik
 sudo docker stack deploy -c ./wordpress-marquinhos/docker-compose.yml  traefik
 sudo docker stack deploy -c ./bitwarden/docker-compose.yml  traefik
 sudo docker stack deploy -c ./redis/docker-compose.yml  traefik
-sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik
- 
+sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik 
+sudo docker stack deploy -c ./node-red/docker-compose.yml  traefik
+sudo docker stack deploy -c ./jellyfin/docker-compose.yml  traefik
+
+
+sudo docker stack deploy -c ./iot-metrics/docker-compose.yml  traefik
  
