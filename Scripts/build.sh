c9c1fb75fd6b5bedead8f00978e35549399fb685
akira <christopher.maekawa@gmail.com>
2025-11-22 15:14:16 -0300

    Updates

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -9,6 +9,7 @@ sudo docker stack deploy -c ./mysql/docker-compose.yml traefik
 sudo docker stack deploy -c ./mariaDB/docker-compose.yml traefik  
 sudo docker stack deploy -c ./influxdb/docker-compose.yml traefik  
 sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
+sudo docker stack deploy -c ./iot-metrics/docker-compose.yml traefik  
 sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
 sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik 
 sudo docker stack deploy -c ./wordpress-marquinhos/docker-compose.yml  traefik
@@ -17,6 +18,10 @@ sudo docker stack deploy -c ./redis/docker-compose.yml  traefik
 sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik 
 sudo docker stack deploy -c ./node-red/docker-compose.yml  traefik
 sudo docker stack deploy -c ./jellyfin/docker-compose.yml  traefik
+
+sudo docker stack deploy -c ./GitLab/docker-compose.yml  traefik
+sudo docker stack deploy -c ./GitLab-Runners/docker-compose.yml  traefik
+sudo docker stack deploy -c ./postgre/docker-compose.yml  traefik
 
 
 sudo docker stack deploy -c ./iot-metrics/docker-compose.yml  traefik
