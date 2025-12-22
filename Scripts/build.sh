5984814325bf19d548d14b43c90f8fa41aedee36
akira <christopher.maekawa@gmail.com>
2025-12-22 14:17:43 -0300

    update

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,27 +2,25 @@
 clear
     
 
-sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
+# sudo docker stack deploy -c ./adguard/docker-compose.yml traefik  
 sudo docker stack deploy -c ./portainer/docker-compose.yml traefik 
 sudo docker stack deploy -c ./Who/docker-compose.yml  traefik
 sudo docker stack deploy -c ./mysql/docker-compose.yml traefik  
 sudo docker stack deploy -c ./mariaDB/docker-compose.yml traefik  
 sudo docker stack deploy -c ./influxdb/docker-compose.yml traefik  
 sudo docker stack deploy -c ./traefik/docker-compose.yml traefik  
-sudo docker stack deploy -c ./iot-metrics/docker-compose.yml traefik  
-sudo docker stack deploy -c ./grafana/docker-compose.yml traefik    
-sudo docker stack deploy -c ./wordpress/docker-compose.yml  traefik 
-sudo docker stack deploy -c ./wordpress-marquinhos/docker-compose.yml  traefik
+# sudo docker stack deploy -c ./iot-metrics/docker-compose.yml traefik  
+sudo docker stack deploy -c ./grafana/docker-compose.yml traefik     
 sudo docker stack deploy -c ./bitwarden/docker-compose.yml  traefik
 sudo docker stack deploy -c ./redis/docker-compose.yml  traefik
-sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik 
-sudo docker stack deploy -c ./node-red/docker-compose.yml  traefik
-sudo docker stack deploy -c ./jellyfin/docker-compose.yml  traefik
+# sudo docker stack deploy -c ./guugru/docker-compose.yml  traefik 
+# sudo docker stack deploy -c ./node-red/docker-compose.yml  traefik
+# sudo docker stack deploy -c ./jellyfin/docker-compose.yml  traefik
 
 sudo docker stack deploy -c ./GitLab/docker-compose.yml  traefik
 sudo docker stack deploy -c ./GitLab-Runners/docker-compose.yml  traefik
 sudo docker stack deploy -c ./postgre/docker-compose.yml  traefik
 
 
-sudo docker stack deploy -c ./iot-metrics/docker-compose.yml  traefik
+# sudo docker stack deploy -c ./iot-metrics/docker-compose.yml  traefik
  
